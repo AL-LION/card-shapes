fac09214e963998e612d362e1e7a7e56760988e2
AL-LION <ramiral0604@gmail.com>
2026-07-20 16:27:53 -0400

    Remove plugin template files

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-export { ExampleTransformer } from "./transformer";
-export { ExampleFilter } from "./filter";
-export { ExampleEmitter } from "./emitter";
 export { default as CardShapes } from "./components/CardShapes";
 
-export type {
-  ExampleTransformerOptions,
-  ExampleFilterOptions,
-  ExampleEmitterOptions,
-} from "./types";
-
-// Re-export shared types from @quartz-community/types
 export type {
   QuartzComponent,
   QuartzComponentProps,
   QuartzComponentConstructor,
-  StringResource,
-  QuartzTransformerPlugin,
-  QuartzFilterPlugin,
-  QuartzEmitterPlugin,
-  QuartzPageTypePlugin,
-  QuartzPageTypePluginInstance,
-  PageMatcher,
-  PageGenerator,
-  VirtualPage,
 } from "@quartz-community/types";
